f0c3504b797e5e5129a5d8c24db0512da050a80f
Juan <jgtaruc@gmail.com>
2016-02-01 10:23:41 -0800

    making the landing page responsive

--- trip_app/static/trip_app/js/login_signup.js
+++ trip_app/static/trip_app/js/login_signup.js
@@ -1,4 +1,5 @@
 function login_active() {
+  $("#login-signup-form").css({opacity: 0, height: "400px"}).animate({opacity: 1.0})
   //document.getElementById('signup-form').style.visibility = "hidden";
   $("#signup-form").css({opacity: 0, visibility: "hidden"}).animate({opacity: 1.0});
   document.getElementById('signup-tab').className = "";
@@ -9,6 +10,7 @@ function login_active() {
 }
 
 function signup_active() {
+  $("#login-signup-form").css({opacity: 0, height: "500px"}).animate({opacity: 1.0})
   //document.getElementById('signup-form').style.visibility = "visible";
   $("#signup-form").css({opacity: 0, visibility: "visible"}).animate({opacity: 1.0});
   document.getElementById('signup-tab').className = "active";
