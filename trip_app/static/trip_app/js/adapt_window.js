86735aeb50198c11154f8738e9ee50d342935a5b
Juan <jgtaruc@gmail.com>
2016-02-02 15:35:29 -0800

    login page done

--- trip_app/static/trip_app/js/adapt_window.js
+++
@@ -1,8 +0,0 @@
-function resize_elements() {
-  if(window.innerWidth < window.innerHeight) {
-    document.getElementById('bg').style.top = "56%";
-  }
-  else {
-    document.getElementById('bg').style.top = "0px";
-  }
-}
--- trip_app/static/trip_app/js/login_signup.js
+++ trip_app/static/trip_app/js/login_signup.js
@@ -2,7 +2,7 @@
 show the login form and hide the signup form, and vice versa for
 signup_active function*/
 function login_active() {
-  $(".login-signup-form").css({opacity: 0, height: "400px"}).animate({opacity: 1.0})
+  $("#login-signup-form").css({opacity: 0, height: "400px"}).animate({opacity: 1.0})
   //document.getElementById('signup-form').style.visibility = "hidden";
   $("#signup-form").css({opacity: 0, visibility: "hidden"}).animate({opacity: 1.0});
   document.getElementById('signup-tab').className = "";
@@ -13,7 +13,7 @@ function login_active() {
 }
 
 function signup_active() {
-  $(".login-signup-form").css({opacity: 0, height: "470px"}).animate({opacity: 1.0})
+  $("#login-signup-form").css({opacity: 0, height: "470px"}).animate({opacity: 1.0})
   //document.getElementById('signup-form').style.visibility = "visible";
   $("#signup-form").css({opacity: 0, visibility: "visible"}).animate({opacity: 1.0});
   document.getElementById('signup-tab').className = "active";
